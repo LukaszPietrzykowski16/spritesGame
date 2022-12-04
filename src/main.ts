5b1de0900ed36e9ea6d69364528434b2e55d2829
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-12-04 19:37:56 +0100

    creating character

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import './style.css'
-import walk from '../sprites/Colour1/NoOutline/120x80_PNGSheets/_CrounchWalk.png'
-import Canva from './canva'
+import walk from './walk.png'
 
-function update(progress) {
+const canvas = document.getElementById('responsive-canvas') as HTMLCanvasElement;
+const myContext = canvas.getContext('2d');
+
+const img = new Image();        
+img.src = walk;    
+
+img.onload = () => myContext?.drawImage(img, 0, 0);
+
+/*
+function update(progress:number) {
+ 
+
   // Update the state of the world for the elapsed time since last render
 }
 
@@ -12,7 +22,7 @@ function draw() {
 
 function loop(timestamp: number) {
   var progress = timestamp - lastRender
-  console.log(progress)
+ 
   update(progress)
   draw()
 
@@ -21,3 +31,40 @@ function loop(timestamp: number) {
 }
 let lastRender = 0
 window.requestAnimationFrame(loop)
+*/
+interface CharacterInterface {
+  spritesheet: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  numberOfFrames: number;
+}
+
+class Character implements CharacterInterface {
+
+  spritesheet: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  timePerFrame: number;
+  numberOfFrames: number;
+  
+
+  constructor(spritesheet: string, x: number, y: number, width: number, height: number, timePerFrame: number, numberOfFrames: number) {
+    this.spritesheet = spritesheet;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.timePerFrame = timePerFrame
+    this.numberOfFrames = numberOfFrames
+  }
+
+
+}
+
+
+const newDeveloper = new Character('tet', 0, 0, 250, 250, 3, 5)
+console.log(newDeveloper)
