336c5db4c98f673abc1b9f19a02e0137768ff23e
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-01-11 18:34:51 +0100

    feat: changing image

--- src/foeClass.ts
+++ src/foeClass.ts
@@ -1,6 +1,36 @@
 import { Character } from "./characterClass";
-import walk from './sprites/walk.png'
-import walkBackwards from './sprites/walk_backwards.png'
+import walk from './sprites/walk_foe.png'
+import walkBackwards from './sprites/walk_backwards_foe.png'
+import jump from './sprites/jump_foe.png'
+import jumpBackwards from './sprites/jump_backwards_foe.png'
+import fall from './sprites/fall_foe.png'
+import fallBackwards  from './sprites/fall_backwards_foe.png'
+import attack from './sprites/attack_foe.png'
+import attackBackwards from './sprites/attack_backwards_foe.png'
+
+/*
+const canvas = document.getElementById('responsive-canvas') as HTMLCanvasElement;
+const myContext = canvas.getContext('2d');
+*/
+
+const imgJump = new Image();        
+imgJump.src = jump;   
+
+const imgJumpBackwards = new Image();        
+imgJumpBackwards.src = jumpBackwards;   
+
+const imgFall = new Image();        
+imgFall.src = fall;   
+
+const imgFallBackwards = new Image();        
+imgFallBackwards.src = fallBackwards;   
+
+const imgAttack = new Image();        
+imgAttack.src = attack;   
+
+const imgAttackBackwards = new Image();        
+imgAttackBackwards.src = attackBackwards;  
+
 const imgRight = new Image();        
 imgRight.src = walk;    
 
